ec56277d28b6284f6766fe9d20bdf0976e1d8e41
Mohamed Al-Khawam <mhdk@larsa.org>
2021-08-07 20:21:33 +0200

    dfdf

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
+const crypto = require('crypto');
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -121,7 +122,6 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
       message: 'Token send to email!',
     });
   } catch (error) {
-    console.log(error);
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
@@ -133,4 +133,32 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     );
   }
 });
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hexa');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gr: Date.now() },
+  });
+
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired', 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfermation = req.body.passwordConfermation;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+  // 3) Update changedPasswordAt property for user
+  // 4) Log the user in, send jwt
+});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/authController');
 router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.signin);
 router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword/:token').post(authController.resetPassword);
+router.route('/resetPassword/:token').patch(authController.resetPassword);
 // router
 //   .route('/')
 //   .get(userController.getAllUsers)
